a7ee0b6252a30f3d18352503ed3ba935c3bbed3c
SaraRoberts <sara.roberts@selu.edu>
2018-07-04 17:36:40 -0500

    Added EF and Models

---
+++ KSS/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KSS.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public decimal Price { get; set; }
+        public String location { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset EndTime { get; set; }
+        public String Prerequisite { get; set; }
+    }
+}
---
+++ KSS/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KSS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String ConfirmPassword { get; set; }
+        public String FName { get; set; }
+        public String LName { get; set; }
+        public int CellNumber { get; set; }
+        public int HomeNumber { get; set; }
+    }
+}
